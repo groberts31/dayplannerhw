fc7d359bbb9f7c33856f7ac1c8f5c8975dd9f4b1
Greg Roberts <groberts3104@gmail.com>
2019-11-13 22:56:58 -0500

    added onclick function for save btns

---
+++ script.js
@@ -0,0 +1,8 @@
+
+var infoBoxEl = document.querySelector(".infoBox").value;
+
+$(".saveBtns").click(function() {
+    // $(".infoBox").localStorage.setItem();
+    console.log("Yes it works");
+})
+
