b9a822dd093aa3110ed2e3d5c2fe519e9fd20908
Greg Roberts <groberts3104@gmail.com>
2019-11-17 14:07:51 -0500

    added date and time using moment.js

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 
 var infoBoxEl = document.querySelector(".infoBox").value;
 
-$(".saveBtns").click(function() {
+$(".saveBtns").click(function () {
     // $(".infoBox").localStorage.setItem();
     console.log("Yes it works");
-})
+});
 
+$(document).ready(function () {
+    var $scope = {};
+    $scope.date = new Date();
+    $scope.moment = moment($scope.date);
+
+    // DateTime Format
+    $('#currentDay').html($scope.moment.format('MMMM Do h:mm:ss a'));
+
+})
