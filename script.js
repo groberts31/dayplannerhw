7e625787ed9a94ebfb0f977d23d0843048de4455
Greg Roberts <groberts3104@gmail.com>
2019-11-17 18:13:08 -0500

    removed moment.js, added function for rows to change when time changes

--- script.js
+++ script.js
@@ -1,17 +1,138 @@
 
-var infoBoxEl = document.querySelector(".infoBox").value;
+var inputVal = $("#save-info").val();
+var saveBtnsEl = document.getElementsByClassName("saveBtns");
+var row1El = $("#row1");
+var row2El = $("#row2");
+var row3El = $("#row3");
+var row4El = $("#row4");
+var row5El = $("#row5");
+var row6El = $("#row6");
+var row7El = $("#row7");
+var row8El = $("#row8");
+var row9El = $("#row9");
 
-$(".saveBtns").click(function () {
-    // $(".infoBox").localStorage.setItem();
-    console.log("Yes it works");
+
+$(document).ready(function () {
+    $(saveBtnsEl).click(function () {
+        if (inputVal !== "") {
+            $('.alerts').text('Please enter info first');
+        }
+        else {
+            $.jStorage.set(inputVal);
+            $.jStorage.get(inputVal);
+        }
+
+    })
 });
+
 
 $(document).ready(function () {
     var $scope = {};
     $scope.date = new Date();
     $scope.moment = moment($scope.date);
+    var hr = "h";
+
 
     // DateTime Format
     $('#currentDay').html($scope.moment.format('MMMM Do h:mm:ss a'));
 
+    if (hr === 9){
+        $(row1El).css("background-color", "green");
+    }
+    else if (hr > 9) {
+        $(row1El).css("background-color", "blue");
+    }
+    else {
+        $(row1El).css("background-color", "red",);
+    };
+    if (hr === 10){
+        $(row2El).css("background-color", "green");
+    }
+    else if (hr > 10) {
+        $(row2El).css("background-color", "blue");
+    }
+    else {
+        $(row2El).css("background-color", "red",);
+    };
+    if (hr === 11){
+        $(row3El).css("background-color", "green");
+    }
+    else if (hr > 11) {
+        $(row3El).css("background-color", "blue");
+    }
+    else {
+        $(row3El).css("background-color", "red",);
+    };
+    if (hr === 12){
+        $(row4El).css("background-color", "green");
+    }
+    else if (hr > 12) {
+        $(row4El).css("background-color", "blue");
+    }
+    else {
+        $(row4El).css("background-color", "red",);
+    };
+    if (hr === 1) {
+        $(row5El).css("backgruond-color", "green");
+    }
+    else if (hr >= 9 && hr <= 12) {
+        $(row5El).css("background-color", "blue");
+    }
+    else {
+        $(row5El).css("background-color", "red");
+    };
+    if (hr === 2) {
+        $(row6El).css("backgruond-color", "green");
+    }
+    else if (hr >= 9 && hr <= 12) {
+        $(row6El).css("background-color", "blue");
+    }
+    else if (hr === 1) {
+        $(row6El).css("background-color", "blue");
+    }
+    else {
+        $(row6El).css("background-color", "red");
+    };
+    if (hr === 3) {
+        $(row7El).css("backgruond-color", "green");
+    }
+    else if (hr >= 9 && hr <= 12) {
+        $(row7El).css("background-color", "blue");
+    }
+    else if (hr < 3) {
+        $(row7El).css("background-color", "blue");
+    }
+    else {
+        $(row7El).css("background-color", "red");
+    };
+    if (hr === 4) {
+        $(row8El).css("backgruond-color", "green");
+    }
+    else if (hr >= 9 && hr <= 12) {
+        $(row8El).css("background-color", "blue");
+    }
+    else if (hr < 4) {
+        $(row8El).css("background-color", "blue");
+    }
+    else {
+        $(row8El).css("background-color", "red");
+    };
+    if (hr === 5) {
+        $(row9El).css("backgruond-color", "green");
+    }
+    else if (hr >= 9 && hr <= 12) {
+        $(row9El).css("background-color", "blue");
+    }
+    else if (hr < 5) {
+        $(row9El).css("background-color", "blue");
+    }
+    else {
+        $(row9El).css("background-color", "red");
+    };
+
+
+
+
+
 })
+
